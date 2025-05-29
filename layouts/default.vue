0452fb2bed70786615b4229db7737b9054aad66b
Claudio Mendonca <claudioccm@gmail.com>
2025-05-29 10:32:26 -0700

    clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,19 +5,5 @@
 </template>
 
 <style lang="scss">
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-  color: #ffffff;
-  background-color: #000000;
-}
 
-.layout-default {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
 </style> 
